8380f9fb21d29fba63568aac22062a9c8f7bf699
Paul Terpstra <Paul Terpstra@DESKTOP-Q0B2K0I>
2023-06-09 13:26:15 -0500

    Add project files.

---
+++ TrophyPrints/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrophyPrints.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; } //Check if string appropriate for password
+    }
+}
